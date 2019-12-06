890149fee5652e336445b75ea4ad22500e9a4ac8
NPGonzalez <naypili@gmail.com>
2019-12-06 02:22:55 -0300

    Se muestran personajes en pantalla

--- src/data.js
+++ src/data.js
@@ -1,40 +1,33 @@
 //FUNCIÓN PARA MOSTRAR PERSONAJES
-export let showValues= (personaje)=>{
-  let allCharactersPotter=[];
-    for(let i=0; i<=(personaje.length-1); i++){ 
-        personajesPotter[i]=personaje[i].name +  personaje[i].house +  personaje[i].ascentry+  personaje[i].species;
-      };   
-      allCharactersPotter=personajesPotter.join('');
-    return allCharactersPotter;
+export const showValues= (POTTER)=>{ 
+
+  POTTER.map(element => {
+   Object.element+=
+  `<img src = ${element.image}>
+  <h1>${element.name }</h1> 
+  <p>${element.house}</p> 
+  <p> ${element.species}</p> `
+
+  document.getElementById("characters").innerHTML=Object.element;
+  console.log(element);
+
+  }); 
 };
 
+//FUNCIÓN PARA FILTRAR POR CASA
+export const filterData= (POTTER,characterByHouse)=>{ 
 
-//FUNCIÓN FILTRADO DE PERSONAJES POR CASA
-export const filterData = (POTTER,sortBy)=>{
-  let filterCharacters;
-  let j=-1;
-  let nameCharactersPotter=[];
-  let houseCharactersPotter=[];
+ if(characterByHouse!=="All"){
+  let filtered = POTTER.filter(element => {element.house==characterByHouse });
 
-
-if(sortBy=='Todos'){ //SE MUESTRAN TODOS LOS PERSONAJES Y SUS CARACTERÍSTICAS CUANDO EL SELECTOR ESTÁ EN  TODOS
-  for(let i=0; i<=(POTTER.length-1); i++){ 
-    //personajesPotter[i]=POTTER[i].name +  POTTER[i].house +  POTTER[i].ascentry+  POTTER[i].species;
-    nameCharactersPotter[i]=POTTER[i].name;
-    houseCharactersPotter[i]=POTTER[i].house;
-         };   
+  document.getElementById("characters").innerHTML=filtered;
+ }
+ else{
+  document.getElementById("characters").innerHTML=[];
+  showValues(POTTER);
 }
-else{ //SE MUESTRAN TODOS LOS PERSONAJES Y SUS CARACTERÍSTICAS CUANDO EL SELECTOR ESTÁ EN ALGUNA DE LAS CUATRO CASAS
-for(let i=0; i<=(POTTER.length-1); i++){
-  j++;
-  if(POTTER[i].house==sortBy){
-    nameCharactersPotter[j]=POTTER[i].name ;
-   }
-};
+     
+  };
 
-};
-filterCharacters= nameCharactersPotter.join(''); 
-return (filterCharacters);
-
-};
-
+  
+  
