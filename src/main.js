cb2d8e4e8c6a6029f28ed6423f6559f7038e5bb3
NPGonzalez <naypili@gmail.com>
2019-12-11 10:10:42 -0300

    Muestra datos, filtra, ordena, flexbox

--- src/main.js
+++ src/main.js
@@ -1,16 +1,38 @@
 import POTTER from './data/potter/potter.js';
 import * as modulo  from './data.js';
-import potter from './data/potter/potter.js';
-//document.body.onload=modulo.showValues(POTTER);
 
-//Filtrado selección
- let characterByHouse = document.getElementById('houses');
- characterByHouse.addEventListener('change', modulo.filterData(POTTER,characterByHouse.value));
- console.log(characterByHouse.value);
+//LLAMADO FUNCIÓN MOSTRAR DATOS
+window.onload = function() {
+  //document.getElementById("results").innerHTML= modulo.showValues(POTTER);
+  document.getElementById('results').innerHTML = modulo.dataSort(POTTER,'ascendent');
+};
+
+
+
+
+//LLAMADO FUNCIÓN DE FILTRADO POR CASA
+document.getElementById('houses').addEventListener('change', function(){
+   document.getElementById('results').value='';
+   document.getElementById('results').innerHTML = modulo.filterData(POTTER,document.getElementById('houses').value);
+});
+
+//LLAMADO FUNCIÓN DE FILTRADO POR ESPECIE
+document.getElementById('characterSpecies').addEventListener('change', function(){
+   document.getElementById('results').value='';
+   document.getElementById('results').innerHTML = modulo.filterData(POTTER,document.getElementById('characterSpecies').value);
+});
+
+//LLAMADO FUNCIÓN DE FILTRADO POR GÉNERO
+document.getElementById('characterGender').addEventListener('change', function(){
+   document.getElementById('results').value='';
+   document.getElementById('results').innerHTML = modulo.filterData(POTTER,document.getElementById('characterGender').value);
+});
  
- // let select = document.getElementById('casa');
-  //let selectedOption = document.getElementById('characteresFilter');
-  //selectedOption.innerHTML= modulo.filterData();
+//LLAMADO FUNCIÓN PARA ORDENAR
+document.getElementById('sortCharacters').addEventListener('change', function(){
+document.getElementById('results').innerHTML = modulo.dataSort(POTTER,document.getElementById('sortCharacters').value);
+});
+  
 
 
 
